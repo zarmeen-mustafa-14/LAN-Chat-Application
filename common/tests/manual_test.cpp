b0988538d0b7a864f1673d00b768b5d38572f811
Abdur Rehman Khan <4pjvr9yfw6@privaterelay.appleid.com>
2026-07-27 09:33:27 -0700

    Add tests for Parser round-trip and escaping edge cases

--- common/tests/manual_test.cpp
+++ common/tests/manual_test.cpp
@@ -1,5 +1,6 @@
-#include "Message.h"
-#include "Serializer.h"
+#include "../Message.h"
+#include "../Serializer.h"
+#include "../Parser.h"
 #include <iostream>
 #include <ctime>
 
@@ -18,5 +19,23 @@ int main()
               << " (timestamp: " << privateMsg.getTimestamp() << ")" << std::endl;
     std::string wireFormat = Serializer::serialize(privateMsg);
     std::cout << "[SERIALIZED] " << wireFormat << std::endl;
+    Message parsedBack = Parser::parse(wireFormat);
+    std::cout << "[PARSED BACK] " << parsedBack.getSenderName()
+              << " -> " << parsedBack.getRecipientName()
+              << ": " << parsedBack.getContent()
+              << " (timestamp: " << parsedBack.getTimestamp() << ")" << std::endl;
+    Message trickyMsg(Protocol::MessageType::CHAT, "Bob", "price is $5|10 total", now);
+    std::string wireFormat2 = Serializer::serialize(trickyMsg);
+    std::cout << "[SERIALIZED TRICKY] " << wireFormat2 << std::endl;
+
+    Message parsedTricky = Parser::parse(wireFormat2);
+    std::cout << "[PARSED TRICKY] content: " << parsedTricky.getContent() << std::endl;
+
+    Message backslashMsg(Protocol::MessageType::CHAT, "Bob", "path is C:\\Users\\Bob", now);
+    std::string wireFormat3 = Serializer::serialize(backslashMsg);
+    std::cout << "[SERIALIZED BACKSLASH] " << wireFormat3 << std::endl;
+
+    Message parsedBackslash = Parser::parse(wireFormat3);
+    std::cout << "[PARSED BACKSLASH] content: " << parsedBackslash.getContent() << std::endl;
     return 0;
 }
