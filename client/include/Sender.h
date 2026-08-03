36554b85e8327fd4e3483428891148b1d1d38b97
Zarmeen Mustafa <zarmeenmustafa14@gmail.com>
2026-08-03 13:23:59 +0500

    Declared Sender.h

---
+++ client/include/Sender.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "server/include/Socket.h"
+#include "common/include/Message.h"
+
+class Sender {
+private:
+    Socket& m_socket;
+public:
+    // Constructor and Destructor
+    explicit Sender(Socket& socket);
+    ~Sender() = default;
+
+    // Delete copy constructor and assignment operator to prevent copying
+    Sender(const Sender&) = delete;
+    Sender& operator=(const Sender&) = delete;
+
+    bool sendMessage(const Message& message);
+};
